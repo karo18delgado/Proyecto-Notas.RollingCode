69935971a5f28732c5030a31afd847f8d20d77c0
Carolina <karo18delgado@gmail.com>
2021-03-18 20:06:17 -0300

    Agregue ID (alfanumerico) + reset en formTask

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 document.getElementById("formTask").addEventListener("submit", saveTask);
 
+function generarId() {
+    return '_' + Math.random().toString(36).substr(2,9);
+};
 function saveTask(e) {
-    
     let title =  document.getElementById("title").value;
     let description = document.getElementById("description").value;
-
     const task = {
+        id: generarId(),
         title,
-        description
+        description,
     };
     if (localStorage.getItem("tasks") === null) {
         let tasks = [];
@@ -21,7 +23,8 @@ function saveTask(e) {
 
     getTasks();
     e.preventDefault();
-}
+    formTask.reset();
+};
 function getTasks() {
     let tasks = JSON.parse(localStorage.getItem("tasks"));
     let tasksView = document.getElementById("tasks");
@@ -32,20 +35,18 @@ function getTasks() {
         let title = tasks[i].title;
         let description = tasks[i].description;
         
-        tasksView.innerHTML += `<div class= "container">
-        <div class="row m-2">
-        <div class="col-sm">
-        <div class="card">
-        <div class="card-body">
-        <h5 class="card-title">${title}</h5>
-        <p class="card-text">${description}</p>
-        <a class="btn btn-danger" onclick="deleteTask('${title}')" >Borrar</a>
-        </div>
-        </div>
-        </div>
+        tasksView.innerHTML += `
+        <div class="col-sm-6">
+            <div class="card m-4">
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}</p>
+                    <a class="btn btn-danger" onclick="deleteTask('${title}')" >Borrar</a>
+                </div>
+            </div>
         </div>`
     }
-}
+};
 function deleteTask(title) {
 let tasks = JSON.parse (localStorage.getItem("tasks")) ;
 for (let i=0; i<tasks.length; i++) {
@@ -55,7 +56,7 @@ for (let i=0; i<tasks.length; i++) {
 }
 localStorage.setItem("tasks", JSON.stringify(tasks));
 getTasks();
-}
+};
 getTasks();
 
 
